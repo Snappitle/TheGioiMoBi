5cc9f10582377e9edadaa739496ccc6204332c6f
Snappitle <ntlinhvi1997@gmail.com>
2018-01-03 01:28:08 +0700

    tạo form, thêm sửa

--- application/views/templates/admin/order_management.php
+++ application/views/templates/admin/order_management.php
@@ -9,7 +9,7 @@
                 <div class="col-md-10"><input type="text" name="user" placeholder="Đơn hàng..." class="form-control box-shadow" id="text" name="btUser"></div>
                 <div class="col-md-1" id="button"><button type="button" class="btn btn-default" name="btSearch"><span class="glyphicon glyphicon-search"></span></button></div>
             </div>
-            <div class="row"><a href="#"><button type="button" class="btn btn-primary"id="bt4">Thêm</button></a></div>
+            <div class="row"><a href="##"><button type="button" class="btn btn-primary"id="bt4">Thêm</button></a></div>
             <div class="row"><a href="#"><button type="button" class="btn btn-primary"id="bt4">Xóa</button></a></div>
             <div class="row"><a href="#"><button type="button" class="btn btn-primary"id="bt4">Sửa</button></a></div>
         </div>
