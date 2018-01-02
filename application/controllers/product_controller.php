d23061369a02dfcc1d9c83bb565b7276b609e87c
Snappitle <ntlinhvi1997@gmail.com>
2018-01-03 01:24:29 +0700

    Revert "form thêm sửa"
    This reverts commit 44176e9bf907b6aaee562d090bbc7d664950deac.

--- application/controllers/product_controller.php
+++ application/controllers/product_controller.php
@@ -1,4 +1,3 @@
-
 <?php
     class product_controller extends CI_Controller
     {
@@ -34,6 +33,15 @@
                 $data["list"] = $this->product_model->getlistbykeyword($keyword);
                 $this->load->view("templates/contents/search",$data);    
             }
+            $this->load->view("footer");
+        }
+
+        public function filter()
+        {
+            $this->load->view("header");
+            $this->load->view("sidebar");
+            $data["list"] = $this->product_model->getfilterlist($_POST["brand"]);
+            $this->load->view("templates/contents/search",$data);
             $this->load->view("footer");
         }
     }
