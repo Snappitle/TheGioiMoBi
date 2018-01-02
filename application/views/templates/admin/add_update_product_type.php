44176e9bf907b6aaee562d090bbc7d664950deac
Snappitle <ntlinhvi1997@gmail.com>
2018-01-03 01:18:18 +0700

    form thêm sửa

---
+++ application/views/templates/admin/add_update_product_type.php
@@ -0,0 +1,10 @@
+<div class="row" id="vienda" style="margin-left: 10px">
+    <div class="col-md-2" id="font14"><lable>Loại: </lable></div>
+    <div class="col-md-3" id="login"><input type="text" name="type" class="form-control box-shadow"/></div>
+    <div class="col-md-2" id="font14"><lable>Giá tiền trở lên:</lable></div>
+    <div class="col-md-3" id="login"><input type="text" name="price" class="form-control box-shadow"/></div>
+</div>
+<div class="row" id="vienda" style="margin-left: 10px">
+    <div class="col-md-5"></div>
+    <div class="col-md-2" id="font14"><button type="button" class="btn btn-primary"id="bt4" style="margin-left: 217px">Lưu</button></div>
+</div>
